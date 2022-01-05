defa2dccf77c7f1bbec4b4ca78674ed200920b4d
minhazpro3 <minhazfr8@gmail.com>
2022-01-05 15:53:19 +0600

    post and get teachers

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const { MongoClient } = require('mongodb');
-// const fileUpload= require('express-fileupload');
+const fileUpload= require('express-fileupload');
 const ObjectId = require('mongodb').ObjectId;
 const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 require('dotenv').config()
-// app.use(fileUpload())
+app.use(fileUpload())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z45ex.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -21,22 +21,43 @@ async function run() {
       await client.connect();
       const database = client.db('studyTeamScic');
       const teachers = database.collection('teachers');
-      const classes = database.collection('classes');
+      // const classes = database.collection('classes');
 
 
 
-    //   post teacher
-    app.post('/teacher', async (req,res)=>{
-        const query = req.body;
-        const result = await teachers.insertOne(query)
-        res.send(result)
-    })
+        // add teachers
+        app.post('/teachers', async (req,res)=>{
+          const name= req.body.name;
+          const education = req.body.education;
+          const subject = req.body.subject;
+          const phone = req.body.phone;
+          const about = req.body.about;
+          const link = req.body.link;
+          const picture = req.files.image;
+          const pictureData = picture.data;
+          const encodedPicture = pictureData.toString('base64')
+          const imageBuffer = Buffer.from(encodedPicture, 'base64')
+          const services = {
+            name,
+            education,
+              subject,
+              phone,
+              about,
+              link,
+              image: imageBuffer
+          }
+          const result = await teachers.insertOne(services)
+          
+          res.json(result)
+      })
 
-      //   get all product
-    app.get('/allData', async (req,res)=>{
+  
+
+      //   get all teachers
+      app.get('/allTeachers', async (req,res)=>{
         const result = await teachers.find({}).toArray()
         res.send(result)
-    })
+      })
 
       
      
